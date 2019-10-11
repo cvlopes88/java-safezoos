f91d84803e76e142fd0db0d29a1a0b9ee45d7fe4
unknown <cvlopes88@gmail.com>
2019-10-10 20:55:20 -0400

    most

--- safezoos/src/main/java/com/lambdaschool/zoos/controller/ZooController.java
+++ safezoos/src/main/java/com/lambdaschool/zoos/controller/ZooController.java
@@ -5,6 +5,7 @@ import com.lambdaschool.zoos.service.ZooService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class ZooController
     private ZooService zooService;
 
     // GET: localhost:2019/zoos/zoos
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping(value = "/zoos", produces = {"application/json"})
     public ResponseEntity<?> listAllZoos()
     {
